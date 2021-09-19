c32c62557ac2bfb0dd6d4b72994a7875fd608cd8
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-18 20:35:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,40 +23,6 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-resource "google_compute_instance" "default" {
-  name         = "demo11"
-  machine_type = "f1-micro"
-  zone         = "us-west2-b"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-  }
-}
-
-resource "google_compute_instance" "default" {
-  name         = "demo2"
-  machine_type = "f1-micro"
-  zone         = "us-west2-b"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-  }
-}
-
 resource "google_compute_instance" "default1" {
   name         = "demo3"
   machine_type = "f1-micro"
