e8fd5e6925c430b3eed28b2240c2401ad800741c
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-19 20:51:28 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   zone         = "us-west4-b"
 
-#code
+#cod
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
