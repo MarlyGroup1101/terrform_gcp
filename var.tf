672db74f9533f5bd0796d3fea315521ca326fa83
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-23 16:54:44 -0700

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,3 +8,20 @@ variable "REGION" {
   default = "us-west4"
 }
 
+variable "machine_size" {
+  type        = "string"
+  description = "The size that this instance will be."
+  default     = "f1-micro"
+}
+
+variable "image_name" {
+  type        = "string"
+  description = "The kind of VM this instance will become"
+  default     = "ubuntu-os-cloud/ubuntu-1804-bionic-v20190403"
+}
+
+variable "script_path" {
+  type        = "string"
+  description = "Where is the path to the script locally on the machine?"
+}
+
