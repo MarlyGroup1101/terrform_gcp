8e9d5b6ba4a97b2ef91010b628cb64b09bd20860
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-23 16:54:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,49 @@
+provider "google" {
+  credentials = file("service-account.json")
+  project     = "just-amp-325919"
+  region      = "us-west4"
 
+}
+
+# Locks the version of Terraform for this particular use case
+terraform {
+  required_version = "~>0.12.0"
+}
+
+# This creates the google instance
 resource "google_compute_instance" "vm_instance" {
-  name         = "cred_cloud"
-  machine_type = "f1-micro"
-  zone         = "us-west4-b"
+  name         = "terraform-instance"
+  machine_type = var.machine_size
 
-#cod
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = var.image_name
     }
   }
 
   network_interface {
-    network = "default"
+    network       = "default"
 
+    # Associated our public IP address to this instance
+    access_config = {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+
+  # We connect to our instance via Terraform and remotely executes our script using SSH
+  provisioner "remote-exec" {
+    script = var.script_path
+
+    connection {
+      type        = "ssh"
+      host        = google_compute_address.static.address
+      user        = var.username
+      private_key = file("service-account.json")
+    }
   }
 }
 
-#works and can create VM in play environments
-
-module "agent_policy" {
-  source     = "terraform-google-modules/cloud-operations/google//modules/agent-policy"
-  version    = "~> 0.2.1"
-  project_id = "just-amp-325919"
-  policy_id  = "ops-agents-policy"
-
-  agent_rules = [
-    {
-      type               = "ops-agent"
-      version            = "current-major"
-      package_state      = "installed"
-      enable_autoupgrade = true
-    },
-  ]
-  os_types = [
-    {
-      short_name = "centos"
-    },
-    {
-      short_name = "debian"
-    },
-    {
-      short_name = "rhel"
-    },
-    {
-      short_name = "sles"
-    },
-    {
-      short_name = "sles_sap"
-    },
-    {
-      short_name = "ubuntu"
-    },
-  ]
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
 }
-#new
