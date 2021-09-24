6b24952f8c5c1012caa37adeb865310566407162
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-24 14:55:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
-
-# This creates the google instance
 resource "google_compute_instance" "vm_instance" {
-
-  name         = "terraform-instance"
+  name         = "demoddd1"
   machine_type = "f1-micro"
   zone         = "us-west4-b"
 
+
   boot_disk {
     initialize_params {
-      image = var.image_name
+      image = "debian-cloud/debian-9"
     }
   }
 
   network_interface {
-    network       = "default"
+    network = "default"
 
   }
-
 }
