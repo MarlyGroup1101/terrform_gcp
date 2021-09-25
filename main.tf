1b174052c86174ef2aa9a577bcec69bdfa858d12
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-09-24 19:11:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   zone         = "us-west4-b"
 
-
+# Locks the version of Terraform for this particular use case
+terraform {
+  required_version = "~>0.12.0"
+}
+  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
