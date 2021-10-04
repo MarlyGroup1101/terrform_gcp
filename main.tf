ff59365f0d2d32eb30f5d98d8de8bb2b8c2cb9cd
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-03 20:16:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,22 @@ resource "google_compute_instance" "vm_instance" {
     
   }
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "1101"
+  machine_type = "f1-micro"
+   zone        = "us-west4-b"
+  
+  tags         = ["externalssh","webserver"]
+  
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    
+  }
+}
