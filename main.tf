dc507e4544f33b3e2baf705927df155a96dfd101
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-11-24 11:08:30 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "qwerd111"
-  machine_type = "f1-micro"
-   zone        = "us-west4-b"
 
-  tags         = ["externalssh","webserver"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-7"
-    }
-    
-  }
-}
 
 #Example Usage - Single User
 resource "google_storage_bucket" "bucket" {
